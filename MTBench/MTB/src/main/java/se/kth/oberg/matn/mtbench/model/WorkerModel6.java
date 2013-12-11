516e339e7d8c4dd2ac0cbaf9f560207245a9a928
Patrik Oldsberg <poldsberg@gmail.com>
2013-12-11 11:33:34 +0100

    added result graphs

---
+++ MTBench/MTB/src/main/java/se/kth/oberg/matn/mtbench/model/WorkerModel6.java
@@ -0,0 +1,44 @@
+package se.kth.oberg.matn.mtbench.model;
+
+import android.util.Log;
+
+import java.util.List;
+
+import se.kth.oberg.matn.mtbench.R;
+
+public class WorkerModel6 extends WorkerModel {
+    private static ThreadPool threadPool = new ThreadPool(true, 20);
+
+    @Override
+    public long doWork(WorkSet workSet) {
+        final long start = System.nanoTime();
+
+        for (int i = 0; i < workSet.getCount(); i++) {
+            threadPool.submit(workSet.getWorkItem());
+        }
+
+        List<Double> results = threadPool.getResults();
+
+        double sum = 0;
+        for (Double result : results) {
+            sum += result;
+        }
+
+        final long end = System.nanoTime();
+
+        Log.e("ResultSum", getName() + ": threadCount: " + workSet.getCount() + " sum: " + sum);
+
+        return end - start;
+    }
+
+    @Override
+    public String getName() {
+        return "Thread Pool, limit = 20";
+    }
+
+    @Override
+    public int getDescriptionResource() {
+        return R.string.description_model4;
+    }
+
+}
